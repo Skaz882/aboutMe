92019640b7f9c5c7585f5ebdcdeaee85846db179
Skaz882 <60199239+Skaz882@users.noreply.github.com>
2020-08-24 19:11:57 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Using println
+        System.out.println("Hi, my name is Sarah!");
+        System.out.println("I have been programming for fun since 2014.");
+        System.out.println("I began messing around with the XML code in some of my computer games after observing other people's changes, and eventually learned some SQL.");
+        System.out.println("I've written and published some mods of my own on Steam for my favorite games, and have dozens more that are personal changes.");
+    }
+}
